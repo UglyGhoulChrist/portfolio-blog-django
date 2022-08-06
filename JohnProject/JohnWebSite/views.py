d92deb6053f2c49ed36ce30fe73ad0f51b7aa3fd
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-08-07 02:11:19 +0300

    начал блок Блог и страницу Блоги

--- JohnProject/JohnWebSite/views.py
+++ JohnProject/JohnWebSite/views.py
@@ -4,13 +4,18 @@ from .models import ExtUser, Social, Blog, Project
 if ExtUser.objects.exists():
     extUser = ExtUser.objects.first()  
 
+
+blogs = Blog.objects.all()    
+socials = Social.objects.all()    
+projects = Project.objects.all()    
+
 def home(request):
-    return render(request, 'JohnWebSite/home.html', {'link':1,'extUser':extUser})
+    return render(request, 'JohnWebSite/home.html', {'link':1,'extUser':extUser,'blogs':blogs,'projects':projects,})
 
 
 def blogs(request):
-    return render(request, 'JohnWebSite/blogs.html',{'link':2,})
+    return render(request, 'JohnWebSite/blogs.html',{'link':2,'blogs':blogs,})
 
 
 def works(request):
-    return render(request, 'JohnWebSite/works.html',{'link':3,})
+    return render(request, 'JohnWebSite/works.html',{'link':3,'projects':projects,})
