1c7fcbfdd4990e2505328779e5f1e0d4cd61e3d4
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-08-19 23:48:17 +0300

    война со статик рут

--- JohnProject/settings.py
+++ JohnProject/settings.py
@@ -101,13 +101,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG==False:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
